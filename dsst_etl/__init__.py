f65730605610b429908aea120e4228ea5ab97dde
leej3 <johnleenimh@gmail.com>
2024-11-12 15:15:17 +0000

    switch to uv managed package

--- dsst_etl/__init__.py
+++ dsst_etl/__init__.py
@@ -2,4 +2,30 @@
 DSST ETL Package
 """
 
-__version__ = "0.1.0"
+import os
+
+
+def get_version():
+    try:
+        from . import _version
+
+        return _version.version
+    except ImportError:
+        generate_version_file()
+        from . import _version
+
+        return _version.version
+
+
+def generate_version_file():
+    import pkg_resources
+
+    version = pkg_resources.get_distribution("dsst_etl").version
+    version_file_content = f"version = '{version}'\n"
+
+    version_file_path = os.path.join(os.path.dirname(__file__), "_version.py")
+    with open(version_file_path, "w") as version_file:
+        version_file.write(version_file_content)
+
+
+__version__ = get_version()
